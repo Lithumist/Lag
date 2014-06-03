a7e927c6ce6245017501cb916a1ab2dd05401630
Pedro Custodio <magneticplum@live.co.uk>
2014-06-03 13:50:13 +0100

    got jaws working

---
+++ compile.py
@@ -0,0 +1,5 @@
+# command line utility that takes a javascript file
+# as input, goes though it and replaces #include with
+# the appropriate js files. Also minimises the resulting 
+# large file and saves it.
+
--- runner.js
+++ runner.js
@@ -5,11 +5,31 @@
  * 
 */ 
 
-function init ()
+function stateGame ()
 {
-    gCanvas = document.getElementById('canvasGame');
-    gContext = gCanvas.getContext('2d');
+    this.setup = function ()
+    {
+        player = new jaws.Sprite({ image:"data/gfx/placeholder_1.png", x:20, y:20, width:16, height:16 });
+        player.scaleTo(2);
+    }
     
-    gContext.fillStyle = "#FF0000";
-    gContext.fillRect(0, 0, 640, 480);
+    this.update = function ()
+    {
+        // move player
+        if ( jaws.pressed( "up" ) ) player.y -= 4;
+        if ( jaws.pressed( "down" ) ) player.y += 4;
+        if ( jaws.pressed( "left" ) ) player.x -= 4;
+        if ( jaws.pressed( "right" ) ) player.x += 4;
+    }
+    
+    this.draw = function ()
+    {
+        jaws.clear();
+        player.draw();
+    }
+}
+
+jaws.onload = function() {
+    jaws.assets.add( "data/gfx/placeholder_1.png" );
+    jaws.start( stateGame );
 }
